31897da4a6e3eba02207f51d055c7aebb80b2a27
tanakrid chanburi <tanakrid.c@ku.th>
2019-11-19 06:01:44 +0700

    kill all customer page

--- src/App.js
+++ src/App.js
@@ -1,33 +1,22 @@
 import React from 'react';
 import { BrowserRouter as Router, Switch, Route} from 'react-router-dom';
 import './App.css';
-import ShowWelcomePage from './ShowWelcomePage';
-import DownloadFile from './DownloadFile';
 import './CSS/setImg.css';
 import VendorPage from './VendorPage';
 import ReportOrderPage from './ReportOrderPage';
 import RegistrationForm from './RegistrationForm';
 import ShowOrderListPage from './ShowOrderListPage';
-import CheckCustomerOrderPage from './CheckCustomerOrderPage';
-import LoginWithCheck from './LogInWithCheck';
-import TestUserProfile from './TestUserProfile';
 import ShowWelcomeVendorPage from './ShowWelcomeVendorPage';
-import LoginCustomerPage from './LoginCustomerPage'
 
 function App() {
   return (
       <Router>
         <Switch>
           <Route exact path='/'   component = {ShowWelcomeVendorPage} />
-          <Route path='/check'  component = {DownloadFile} />
           <Route path='/vendor' component = {VendorPage} />
           <Route path="/reportOrderPage" component = {ReportOrderPage} />
-          <Route path="/portalCustomer" component ={LoginWithCheck}/>
           <Route path="/regisCustomer" component ={RegistrationForm}/>
           <Route path="/showOrder" component ={ShowOrderListPage}/>
-          <Route path="/CheckCustomerOrderPage" component={CheckCustomerOrderPage}/>
-          <Route path='/UserAccount'  component = {TestUserProfile} />
-          <Route path='/HomeVendor' component ={ShowWelcomeVendorPage}/>
         </Switch>
        </Router>
 
