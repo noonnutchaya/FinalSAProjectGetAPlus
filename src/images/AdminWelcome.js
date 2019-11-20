71db817eb6c9d46be4f48300afbf1c3bb99e0bb8
tanakrid chanburi <tanakrid.c@ku.th>
2019-11-20 12:28:30 +0700

    add new image

--- src/images/AdminWelcome.js
+++ src/images/AdminWelcome.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 import welcomeBGAdmin from './images/welcomeBGAdmin.png';
+import homeIcon from './images/homeIcon.png';
 import welcomeIcon from './images/enterPageGreen.png';
 import welcomeBGAdmin2 from './images/welcomeBGAdmin2.png';
 import './CSS/setImg.css';
@@ -15,7 +16,7 @@ class AdminWelcome extends React.Component {
                 <div id="SetSent"> หน้าเว็บไซต์สำหรับ Admin </div> */}
                  <img src={welcomeBGAdmin} id ="setBG" />
                 
-                <Link to="/vendor"> <img src={welcomeIcon} id="logoWelcome" />  </Link>
+                <Link to=""> <img src={welcomeIcon} id="logoWelcome" />  </Link>
 
                 <img src={welcomeBGAdmin2} id ="setBG" />
 
